49ead653b2325721a47d2b1974d48dda6062418a
saurabh kumar <93548806+saurabhsoljar@users.noreply.github.com>
2026-01-06 22:53:56 +0530

    conversion data typs

---
+++ 01_Basic/03_conversionOpration.js
@@ -0,0 +1,31 @@
+let score ="50abc"
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "50"=> 50
+//"50abc" => NaN
+
+let isLoggedIn = 0
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
+// 1 => true
+// 0 => false
+
+//"" => false
+// "abc" => true
+// " " => true
+
+
+let someNumber = 1234
+
+let stringNumber = String(someNumber)
+console.log(typeof stringNumber);
+console.log(stringNumber);
+// 1234 => "1234"
