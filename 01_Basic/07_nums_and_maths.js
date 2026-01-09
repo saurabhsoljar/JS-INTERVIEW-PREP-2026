3fb054c2e6f527226d0f3d96716e7420e05f34ea
saurabh kumar <93548806+saurabhsoljar@users.noreply.github.com>
2026-01-09 23:25:01 +0530

    Basics of Numbs

---
+++ 01_Basic/07_nums_and_maths.js
@@ -0,0 +1,25 @@
+const score = 400
+// console.log(score);
+
+
+const balance = new Number(100);
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+const otherNumber = 123.8956;
+// console.log(otherNumber.toPrecision(2));
+
+
+const hundreds = 1000000;
+// console.log(hundreds.toLocaleString("en-IN"));// "10,00,000"
+// console.log(hundreds.toLocaleString("de-DE"));// "1.000.000"
+// console.log(hundreds.toLocaleString("ar-EG"));// "١٬٠٠٠٬٠٠٠"
+
+// console.log(hundreds.toLocaleString("en-IN", {style: "currency", currency: "INR"}));// "₹10,00,000.00"
+
+
+
+
+
