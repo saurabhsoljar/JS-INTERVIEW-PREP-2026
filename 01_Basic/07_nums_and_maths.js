be01846995d8aea5158ff72c761cde4957d45949
saurabh kumar <93548806+saurabhsoljar@users.noreply.github.com>
2026-01-10 15:19:34 +0530

    Maths

--- 01_Basic/07_nums_and_maths.js
+++ 01_Basic/07_nums_and_maths.js
@@ -20,6 +20,24 @@ const hundreds = 1000000;
 // console.log(hundreds.toLocaleString("en-IN", {style: "currency", currency: "INR"}));// "₹10,00,000.00"
 
 
+//========================= MATHS =========================//
+
+// console.log(Math);
+// console.log(Math.abs(-5));
+// console.log(Math.round(5.5));
+// console.log(Math.ceil(5.1));
+// console.log(Math.floor(5.9));
+// console.log(Math.min(5,3,8,1,4));
+// console.log(Math.max(5,3,8,1,4));
+// console.log(Math.random());
+// console.log((Math.random()*10)+1);
+
+const min = 10;
+const max = 20;
+
+console.log(Math.floor(Math.random()*(max-min+1))+min);
+
+
 
 
 
