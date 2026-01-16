fa9a8a4c14ec9c151d9bd62fb24808031de714ab
saurabh kumar <93548806+saurabhsoljar@users.noreply.github.com>
2026-01-16 23:02:28 +0530

    control flor more

--- 04_control_flow/01_one.js
+++ 04_control_flow/01_one.js
@@ -20,10 +20,47 @@ if(2!=3){
 
 const temperature=70
 
-if(temperature < 60){
-    console.log("temperature is less then 60");
+// if(temperature < 60){
+//     console.log("temperature is less then 60");
     
-}else{
-    console.log("temperature is grater the 60");
-}
+// }else{
+//     console.log("temperature is grater the 60");
+// }
 
+//logical operators with if else
+
+
+// const score = 200
+
+// if(score>100){
+//     var power ="fly"
+//     console.log(`user power:${power}`);
+    
+// }
+// console.log(`user power:${power}`);
+
+
+//sorthand notation of if else (ternary operator)
+
+const balance=1000
+//if (balance>500)console.log("testing shorthand if");
+
+// if (balance<500){
+//     console.log("less than");
+// }else if(balance<750){
+//     console.log("less than 750");
+// }else if(balance<10000){
+//     console.log("less than 1001");
+// }
+
+
+const userLoggedIn=true
+const debitCard=true
+const loggedInFroomEmail = true
+//logical AND operator (&&) ,logical OR operator (||) , logical NOT operator (!)
+
+if(userLoggedIn && debitCard){
+    console.log("purchase successful");
+}if(userLoggedIn || loggedInFroomEmail){
+    console.log("user logged in");
+}
---
+++ 04_control_flow/02_switch.js
@@ -0,0 +1,51 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+//const month = 3;
+// switch (month) {
+//     case 1:
+//         console.log("January");
+//         break;
+//     case 2:
+//         console.log("February");
+//         break;
+//     case 3:
+//         console.log("March");
+//         break;
+//     default:
+//         console.log("Not a valid month");
+//         break;
+// }
+
+const month="july"
+
+switch (month) {
+    case "jan":
+        console.log("january");
+        break;
+    case "feb":
+        console.log("febuary");
+        break;
+    case "mar":
+        console.log("march");
+        break;
+    case "may":
+        console.log("maye");
+        break;
+    case "june":
+        console.log("june");
+        break;
+    case "july":
+        console.log("july");
+        break;
+
+    default:
+        console.log("not valid");
+        break;
+}
